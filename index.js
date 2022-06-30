a0fbd3889837bf09702b606afdab68c2ed4fae13
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-06-30 08:24:14 +0600

    mongodb added

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ const port = process.env.PORT || 8080;
 app.use(express.json())
 app.use(cors())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ljopg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect()
+
+        const billingDataCollection = client.db('powerHackData').collection('billingData');
+
+        app.get('/billing-list', async (req,res) => {
+            const page = Number(req.query.page);
+            const cursor = billingDataCollection.find({})
+            const result = await cursor.skip(10*page).limit(10).toArray();
+            const dataCount = await billingDataCollection.estimatedDocumentCount();
+            res.send({message: 'success', data: result, dataCount})
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
 app.get('/', (req,res) => {
     res.send('Node Server is Running')
 });
